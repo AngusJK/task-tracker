afd8157b2c1379f052d703b4f120829c68c60b78
Angus Kellett <anguskellett@hotmail.com>
2021-05-05 01:41:44 +0000

    removes boilerplate, adds hello in h1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Reveal <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, from the people at React (we can see you)</h1>
     </div>
   );
 }
